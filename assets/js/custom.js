e9db2c0a318437033d075528797ec896bc172121
Raj rana <raj.rana507@gmail.com>
2020-09-09 23:50:21 +0530

    portfolio and projects added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,3 +18,81 @@
     });
 }(window.jQuery);
 
+
+
+
+let allProjects = [
+    {
+        technology:'reactjs',
+        project : {
+                title:'Amazon clone',
+                description: 'this is clone of the amazon desktop website with login and add to cart functionality made with reactjs ,firebase and state is managed by react context api',
+                technology: 'Reactjs, Html, Css, firebase',
+                img:"assets/images/amazon.png",
+                projectLink:"https://clone-f8ff9.web.app/"
+        }
+    },
+    {
+        technology:'reactjs',
+        project : {
+                title:'Spotify clone ',
+                description:'this is the spotify desktop clone with login ann view playlist functionality made with react using context Api for state managment hosted with firebase',
+                technology: 'Reactjs, Html, Css, firebase',
+                img:"assets/images/spotify.jpg",
+                projectLink:"https://spotify-clone-b0859.web.app/"
+        }
+    }
+]
+
+
+
+function showProject(type) {
+    let projects = [];
+    if ( type == 'all' ) {
+        projects = [...allProjects]
+        renderProjects(projects);
+    }
+    
+    if (type === 'reactjs') {
+        projects = allProjects.filter(el => el.technology === 'reactjs');
+        renderProjects(projects);
+    }
+}
+function renderProjects(projects) {
+    $(".portfolio__projects").empty()
+    let element = '' 
+    for (let i = 0; i < projects.length; i++) {
+         element += '<div class="portfolio__projectsItem" onclick="openModal('+i+')"><img src="'+projects[i].project.img+'" /><div> '+ projects[i].project.title +' </div></div>';
+    }
+
+    $(".portfolio__projects").append(element);
+
+}
+
+showProject('all');
+
+function openModal(selectedIndex){
+    const selectedProject = allProjects[selectedIndex];
+    console.log('this is the selected project', selectedProject);
+    $('.modal').modal();
+    $('#modal1').modal('open');
+
+    $("#modal1").empty()
+    let project=`<div class="modal__title"> ${selectedProject.project.title} </div> 
+			<div class="modal__description"> ${selectedProject.project.description}  </div> 
+			<div class="modal__projectElements">
+				<div> 
+					<i class="lni lni-link"></i>
+					Preview : 
+					<a href="${selectedProject.project.projectLink}" target="_blank"> ${selectedProject.project.projectLink} </a>
+				</div>
+				<div> 
+                    <i class="lni lni-code"></i>
+                    ${selectedProject.project.technology}
+				</div>
+			</div>
+			<div class="modal__project"> 
+				<img src="${selectedProject.project.img}" /> 
+            </div>`
+    $("#modal1").append(project);
+}
